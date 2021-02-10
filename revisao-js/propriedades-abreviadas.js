d45a2a2108dcaaa93b067551b993c5d16e3e0084
amanda0617 <amanda21limah@gmail.com>
2021-02-10 01:26:23 +0000

    (09/02) Ínicio da revisão de JS

---
+++ revisao-js/propriedades-abreviadas.js
@@ -0,0 +1,16 @@
+
+let usuario = 'joao'
+let senha = 'Deus123'
+let name = 'João das Couves'
+
+let userInfo = {
+    login: usuario,
+    password: senha,
+    name, // Propriedade abreviada (name:name)
+    lastLogin // (lastLogin: lastLogin)
+}
+
+console.log(userInfo)
+
+let info = 'Isto é um teste'
+console.log(info)
