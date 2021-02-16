712bafda38961acf88aa4f513c4295d6e829e48f
amanda0617 <amanda21limah@gmail.com>
2021-02-16 21:52:03 +0000

    arrumando código

--- revisao-js/propriedades-abreviadas.js
+++ revisao-js/propriedades-abreviadas.js
@@ -1,16 +1,20 @@
+// Criando um objeto cujo valor das propriedades vem
+// de variáveis
 
 let usuario = 'joao'
 let senha = 'Deus123'
 let name = 'João das Couves'
+let lastLogin = '2021-02-08 22:49:28'
 
 let userInfo = {
     login: usuario,
     password: senha,
     name, // Propriedade abreviada (name:name)
-    lastLogin // (lastLogin: lastLogin)
+    lastLogin // (lastlogin: lastlogin)
 }
 
 console.log(userInfo)
 
 let info = 'Isto é um teste'
 console.log(info)
+console.log({info}) // tranformando a variável em obj, é possível saber a qual variável pertence determinado valor
